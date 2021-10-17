422d89c602d6628160e5fa8dfb3dc99fca53d1f1
Englert Ervin  Brunó <englert@users.noreply.github.com>
2021-10-17 21:37:40 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,76 @@
+//név;születési_dátum;nemzetiség;rajtszám
+//Lewis Hamilton;1985.01.07;brit;44
+//Nick Heidfeld;1977.05.10;német;
+
+using System;                     // Console
+using System.IO;                  // StreamReader
+using System.Text;                // Encoding
+using System.Collections.Generic; // List<>, Dictionary<>
+using System.Linq;                // from where select
+
+class Pilota{
+    public string név;
+    public string születési_dátum;
+    public string nemzetiség;
+    public int rajtszám;
+    public int év;
+}
+
+class Program {
+    public static void Main (string[] args) {
+        var lista = new List<Pilota>();              
+        
+        var f =  new StreamReader("pilotak.csv", Encoding.UTF8); 
+        var elsosor = f.ReadLine();                   
+        
+        while (!f.EndOfStream){             
+		  var pilota = new Pilota();             
+		  var s = f.ReadLine().Split(';');          
+		  pilota.név             = s[0];                      
+		  pilota.születési_dátum = s[1];                      
+		  pilota.nemzetiség      = s[2];             
+		  pilota.rajtszám        = (s[3].Length>0) ?  int.Parse(s[3]): 0;
+          pilota.év              = int.Parse( s[1].Substring(0,4));             
+		  lista.Add(pilota);                       
+		}
+        f.Close();                          
+
+        // 3. feladat: Dolgozók száma: ? 
+        Console.WriteLine($"3. feladat: {lista.Count} fő");
+
+        // 4. feladat:
+        var utolso_nev = lista.Last().név;
+        Console.WriteLine($"4. feladat: {utolso_nev}");
+
+        // 5. feladat:
+        var xix = ( from sor in lista where sor.év < 1901 select (sor.név, sor.születési_dátum) );
+        Console.WriteLine(    $"5. feladat: ");
+        foreach(var sor in xix){
+            Console.WriteLine($"        {sor.név} ({sor.születési_dátum}.) ");
+        }
+        // 6. feladat legkisebb rajtszám pilótájának nemzetisége    
+        var nemzetiség = ( from sor in lista where sor.rajtszám >0  orderby sor.rajtszám select sor.nemzetiség).First();
+        Console.WriteLine(    $"6. feladat: {nemzetiség}");
+        
+        // 7. feladat
+        var statisztika = new Dictionary<int, int>(); 
+       
+        foreach (var sor in lista) {                     
+            if (statisztika.ContainsKey(sor.rajtszám)) {  
+                statisztika[sor.rajtszám]++;             
+            }   
+            else {                                       
+            statisztika[sor.rajtszám] = 1;               
+            }                                                   
+        }
+
+        Console.Write($"7. feladat:  ");
+        foreach(var kulcs_ertek in statisztika){        
+            if ((kulcs_ertek.Value > 1) & (kulcs_ertek.Key > 0)) {
+                Console.Write($" {kulcs_ertek.Key} ");
+            }
+        }
+        Console.WriteLine();
+
+    }
+}
