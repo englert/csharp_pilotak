4a67a65650c13f823d0f01a7cc296739bd6ad815
Englert Ervin  Brunó <englert@users.noreply.github.com>
2021-10-27 19:29:42 +0000

    group by

--- main.cs
+++ main.cs
@@ -1,7 +1,6 @@
 //név;születési_dátum;nemzetiség;rajtszám
 //Lewis Hamilton;1985.01.07;brit;44
 //Nick Heidfeld;1977.05.10;német;
-
 using System;                     // Console
 using System.IO;                  // StreamReader
 using System.Text;                // Encoding
@@ -9,11 +8,20 @@ using System.Collections.Generic; // List<>, Dictionary<>
 using System.Linq;                // from where select
 
 class Pilota{
-    public string név;
-    public string születési_dátum;
-    public string nemzetiség;
-    public int rajtszám;
-    public int év;
+    public string név               {get; private set;}
+    public string születési_dátum   {get; private set;}
+    public string nemzetiség        {get; private set;}
+    public int rajtszám             {get; private set;}
+    public int év                   {get; private set;}
+
+    public Pilota(string sor){
+        var s = sor.Split(';');
+        név             = s[0];
+        születési_dátum = s[1];
+        nemzetiség      = s[2];
+        rajtszám        = (s[3].Length>0) ?  int.Parse(s[3]): 0;  
+        év              = int.Parse( s[1].Substring(0,4));
+    }
 }
 
 class Program {
@@ -24,14 +32,7 @@ class Program {
         var elsosor = f.ReadLine();                   
         
         while (!f.EndOfStream){             
-		  var pilota = new Pilota();             
-		  var s = f.ReadLine().Split(';');          
-		  pilota.név             = s[0];                      
-		  pilota.születési_dátum = s[1];                      
-		  pilota.nemzetiség      = s[2];             
-		  pilota.rajtszám        = (s[3].Length>0) ?  int.Parse(s[3]): 0;
-          pilota.év              = int.Parse( s[1].Substring(0,4));             
-		  lista.Add(pilota);                       
+		    lista.Add(  new Pilota(f.ReadLine())  );                       
 		}
         f.Close();                          
 
@@ -50,27 +51,21 @@ class Program {
         }
         // 6. feladat legkisebb rajtszám pilótájának nemzetisége    
         var nemzetiség = ( from sor in lista where sor.rajtszám >0  orderby sor.rajtszám select sor.nemzetiség).First();
-        Console.WriteLine(    $"6. feladat: {nemzetiség}");
+        Console.WriteLine( $"6. feladat: {nemzetiség}");
         
         // 7. feladat
-        var statisztika = new Dictionary<int, int>(); 
-       
-        foreach (var sor in lista) {                     
-            if (statisztika.ContainsKey(sor.rajtszám)) {  
-                statisztika[sor.rajtszám]++;             
-            }   
-            else {                                       
-            statisztika[sor.rajtszám] = 1;               
-            }                                                   
-        }
+        var query = (
+            from sor in lista 
+            where sor.rajtszám > 0
+            group sor by sor.rajtszám 
+            into res
+            where res.Count() > 1 
+            select res.Key);
 
         Console.Write($"7. feladat:  ");
-        foreach(var kulcs_ertek in statisztika){        
-            if ((kulcs_ertek.Value > 1) & (kulcs_ertek.Key > 0)) {
-                Console.Write($" {kulcs_ertek.Key} ");
-            }
+        foreach(var i in query){        
+            Console.Write($" {i} ");
         }
         Console.WriteLine();
-
     }
 }
